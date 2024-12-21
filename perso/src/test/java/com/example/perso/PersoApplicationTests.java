78252663a22c474341f0e475a4e28ec8b0dc87b4
Thomas <thomashaulet@gmail.com>
2024-12-21 13:35:06 +0100

    add submodule persoAng

--- perso/src/test/java/com/example/perso/PersoApplicationTests.java
+++ perso/src/test/java/com/example/perso/PersoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Dna2ApplicationTests {
+class PersoApplicationTests {
 
 	@Test
 	void contextLoads() {
